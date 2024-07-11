942c358d39065d9c13e7621d56168cb27532d065
paragmore <paragmore306@gmail.com>
2024-07-11 19:48:23 +0530

    types

---
+++ types/index.d.ts
@@ -0,0 +1,20 @@
+import {
+  getApiBaseUrl,
+  HTTP_METHODS,
+  requestExecutor
+} from '../src/request-executor'
+import {
+  IReply,
+  ApiError,
+  ApiHelperHandler,
+  ApiHelper
+} from '../src/api-helper'
+export {
+  getApiBaseUrl,
+  HTTP_METHODS,
+  requestExecutor,
+  IReply,
+  ApiError,
+  ApiHelperHandler,
+  ApiHelper
+}
